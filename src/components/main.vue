cd9c4ba4c518125230b49cf8b183ba342c85ab84
seokwon <spilown@gmail.com>
2022-04-22 11:01:57 +0900

    add bootstrap5

---
+++ src/components/main.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>bootstrap 5 test</h1>
+
+  <h1 class="display-1">display 1 heading</h1>
+  <h1 class="display-2">display 2 heading</h1>
+  <h1 class="display-6">display 6 heading</h1>
+
+  <h1 class="display-1">Display 1</h1>
+  <h1 class="display-2">Display 2</h1>
+  <h1 class="display-3">Display 3</h1>
+  <h1 class="display-4">Display 4</h1>
+  <h1 class="display-5">Display 5</h1>
+  <h1 class="display-6">Display 6</h1>
+
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+  <p class="lead">
+    This is a lead paragraph. It stands out from regular paragraphs.
+  </p>
+</template>
+
+<script></script>
+
+<style scoped></style>
