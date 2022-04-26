d0590eee839c5b34920284b86d602b6a43a221b1
seokwon <spilown@gmail.com>
2022-04-26 23:18:12 +0900

    testcode

--- src/components/UserManagement.vue
+++ src/components/UserManagement.vue
@@ -18,17 +18,71 @@
     </div>
   </div>
 
-  <div class="panel panel-default">
-    <div class="panel-heading">Panel heading without title</div>
-    <div class="panel-body">Panel content</div>
-  </div>
-
-  <div class="panel panel-default">
+  <div class="panel panel-default" style="margin-top: 20px">
     <div class="panel-heading">
-      <h3 class="panel-title">조건검색</h3>
+      <h3 class="panel-title text-right">조건검색</h3>
     </div>
     <div class="panel-body">Panel content</div>
   </div>
+
+  <div>
+    <div class="row table-bordered" style="margin-bottom: 10px">
+      <div class="col-lg-9 col-md-9">
+        <!-- <h2>[{{ detail.eventId }}] {{ detail.eventName }}</h2> -->
+        event id eventName
+      </div>
+      <div class="col-lg-3 col-md-3">
+        <div
+          class="btn-toolbar"
+          role="toolbar"
+          aria-label="..."
+          style="float: right; margin-top: 15px"
+        >
+          <button
+            type="button"
+            class="btn btn-sm btn-default"
+            @click="clickModify"
+          >
+            수정
+          </button>
+          <button
+            type="button"
+            class="btn btn-sm btn-default"
+            @click="clickRemove"
+          >
+            삭제
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="progress">
+    <div
+      class="progress-bar"
+      role="progressbar"
+      aria-valuenow="60"
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style="width: 60%"
+    >
+      <span class="sr-only">60% Complete</span>
+    </div>
+  </div>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <td>테이블입니다</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>name</td>
+        <td>seokwon</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 <script>
 export default {
@@ -36,6 +90,15 @@ export default {
     return {
       name: "seokwon",
     };
+  },
+
+  methods: {
+    clickRemove() {
+      console.log("remove");
+    },
+    clickModify() {
+      console.log("mod");
+    },
   },
 };
 </script>
